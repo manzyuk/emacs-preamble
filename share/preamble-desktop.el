874f82fbfe4d1b6bed103672c3bef52e82fd4d4c
Oleksandr Manzyuk <manzyuk@gmail.com>
2012-02-23 14:37:03 +0000

    Remove these customizations for the time being.

--- share/preamble-desktop.el
+++ share/preamble-desktop.el
@@ -1,7 +1,4 @@
 ;; Enable `desktop-save-mode'.
-(setq desktop-base-file-name "desktop"
-      desktop-base-lock-name "desktop.lock"
-      desktop-dirname user-emacs-directory)
 (desktop-save-mode 1)
 
 (provide 'preamble-desktop)
