7fd87bcc866c9e28ace71a78709d34acf10f1888
Oleksandr Manzyuk <manzyuk@gmail.com>
2012-02-23 15:50:44 +0000

    Initialize the alist of packages available for activation.

--- share/preamble-package.el
+++ share/preamble-package.el
@@ -3,6 +3,8 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
+(package-initialize)
+
 (when (not package-archive-contents)
   (package-refresh-contents))
 
