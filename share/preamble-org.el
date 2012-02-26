338407fea27fdbdf80d95d23d5ba65db069ba416
Oleksandr Manzyuk <manzyuk@gmail.com>
2012-02-26 13:52:26 +0000

    Another little commentary.

--- share/preamble-org.el
+++ share/preamble-org.el
@@ -15,6 +15,9 @@
 (setq org-capture-templates
       '(("t" "TODO" entry (file "~/org/todo.org") "* TODO %?")))
 
+(setq org-src-fontify-natively t)
+
+;; Don't check spelling inside code blocks and code block delimiters.
 (defun preamble-org-mode-in-block-p ()
   (let ((pos (max (1- (point)) (point-min))))
     (some (lambda (ovl)
@@ -34,7 +37,5 @@
              (not (preamble-org-mode-in-block-delimiter-p)))))
 
 (ad-activate 'org-mode-flyspell-verify)
-
-(setq org-src-fontify-natively t)
 
 (provide 'preamble-org)
