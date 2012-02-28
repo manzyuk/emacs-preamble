574980d047d3477e0a3a009d3b8f2a69e424ada6
Oleksandr Manzyuk <manzyuk@gmail.com>
2012-02-28 17:04:51 +0000

    A commentary.

--- share/preamble-bookmarks.el
+++ share/preamble-bookmarks.el
@@ -1,3 +1,7 @@
+;; Dumbed down, quick, nameless bookmarks.  One could also use marks
+;; instead of bookmarks (set a mark with C-SPC C-SPC and jump to a
+;; mark with C-u C-SPC or C-x C-SPC), but marks are modified by long
+;; movement commands.
 (defun preamble-bookmark-set ()
   (interactive)
   (bookmark-set (buffer-name)))
