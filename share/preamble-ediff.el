54828f283fe303d796a24130639911666c73ff23
Oleksandr Manzyuk <manzyuk@gmail.com>
2012-02-28 15:41:53 +0000

    Split windows horizontally rather than vertically in ediff.

--- share/preamble-ediff.el
+++ share/preamble-ediff.el
@@ -1,4 +1,7 @@
 ;; Do everything in one frame.
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 
+;; Split windows horizontally rather than vertically.
+(setq ediff-split-window-function 'split-window-horizontally)
+
 (provide 'preamble-ediff)
