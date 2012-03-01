d7c78e8071c6aedc121b827f9ca698e1f124aaeb
Oleksandr Manzyuk <manzyuk@gmail.com>
2012-03-01 20:47:08 +0000

    Resurrect `preamble-prog-mode-hook'.

--- share/preamble-prog.el
+++ share/preamble-prog.el
@@ -2,7 +2,10 @@
   (set (make-local-variable 'comment-auto-fill-only-comments) t)
   (auto-fill-mode 1))
 
+(defun preamble-prog-mode-hook ()
+  (preamble-comment-auto-fill-only-comments))
+
 (add-hook 'prog-mode-hook
-          'preamble-comment-auto-fill-only-comments)
+          'preamble-prog-mode-hook)
 
 (provide 'preamble-prog)
