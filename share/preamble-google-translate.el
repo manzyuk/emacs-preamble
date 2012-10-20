272cd577fc0b1ea74187c575ccd0af4a40964d26
Oleksandr Manzyuk <manzyuk@gmail.com>
2012-10-20 17:48:41 +0300

    Update key binding to google-translate.

--- share/preamble-google-translate.el
+++ share/preamble-google-translate.el
@@ -5,6 +5,13 @@
 
 (setq google-translate-enable-ido-completion t)
 
-(global-set-key "\C-ct" 'google-translate-query-translate)
+(global-set-key (kbd "C-c t")
+                'google-translate-at-point)
+(global-set-key (kbd "C-c T")
+                'google-translate-query-translate)
+(global-set-key (kbd "C-c r")
+                'google-translate-at-point-reverse)
+(global-set-key (kbd "C-c R")
+                'google-translate-query-translate-reverse)
 
 (provide 'preamble-google-translate)
