459199afb97416779292b16f33f24f43b3b2aa1a
Oleksandr Manzyuk <manzyuk@gmail.com>
2013-05-14 23:23:41 +0300

    Enable `paredit-mode' in the minibuffer during `eval-expression'.

--- share/preamble-minibuffer.el
+++ share/preamble-minibuffer.el
@@ -8,4 +8,13 @@
 (setq savehist-additional-variables '(search-ring regexp-search-ring))
 (savehist-mode 1)
 
+;; Enable `paredit-mode' in the minibuffer during `eval-expression'.
+(eval-after-load "paredit"
+  '(progn
+     (defun preamble-turn-on-paredit-mode-in-minibuffer ()
+       (when (eq this-command 'eval-expression)
+         (paredit-mode 1)))
+     (add-hook 'minibuffer-setup-hook
+               'preamble-turn-on-paredit-mode-in-minibuffer)))
+
 (provide 'preamble-minibuffer)
